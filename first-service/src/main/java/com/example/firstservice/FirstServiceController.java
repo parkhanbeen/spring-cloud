cb32f314a799961f4340e6980dd4826721141fb8
박한빈 <phb0228@naver.com>
2022-05-14 16:49:26 +0900

    path를 변경하라

--- first-service/src/main/java/com/example/firstservice/FirstServiceController.java
+++ first-service/src/main/java/com/example/firstservice/FirstServiceController.java
@@ -1,8 +1,10 @@
 package com.example.firstservice;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@RequestMapping("/first-service")
 @RestController
 public class FirstServiceController {
 
