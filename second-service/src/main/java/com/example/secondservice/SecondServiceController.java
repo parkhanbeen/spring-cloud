17c9af1f5df351e64afb00fd2796c41f22add2b6
박한빈 <phb0228@naver.com>
2022-05-14 16:50:04 +0900

    라우팅에 맞춰 path를 변경하라

--- second-service/src/main/java/com/example/secondservice/SecondServiceController.java
+++ second-service/src/main/java/com/example/secondservice/SecondServiceController.java
@@ -1,8 +1,10 @@
 package com.example.secondservice;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@RequestMapping("/second-service")
 @RestController
 public class SecondServiceController {
 
