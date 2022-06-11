e8a46d41e6f1ccc15a2210fc12d150642d9796e7
박한빈 <phb0228@naver.com>
2022-06-11 16:49:45 +0900

    actuator 설정을 추가하라

--- user-service/src/main/java/com/example/userservice/security/WebSecurity.java
+++ user-service/src/main/java/com/example/userservice/security/WebSecurity.java
@@ -23,6 +23,7 @@ public class WebSecurity extends WebSecurityConfigurerAdapter {
   protected void configure(HttpSecurity http) throws Exception {
     http.csrf().disable();
 //    http.authorizeHttpRequests().antMatchers("/users/**").permitAll();
+    http.authorizeRequests().antMatchers("/actuator/**").permitAll();
     http.authorizeRequests().antMatchers("/**")
         .hasIpAddress("172.30.1.54")
         .and()
